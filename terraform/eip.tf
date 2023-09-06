22156c1554011cc585f3407afe251d5afc7a25c8
jason <skylarnguyen25@gmail.com>
2023-09-06 20:36:28 +0700

    add gateway/ elastic ip

---
+++ terraform/eip.tf
@@ -0,0 +1,8 @@
+resource "aws_eip" "eip_1" {
+    depends_on = [ aws_internet_gateway.igw ]
+}
+  
+resource "aws_eip" "eip_2" {
+    depends_on = [ aws_internet_gateway.igw ]
+  
+}
---
+++ terraform/ngw.tf
@@ -0,0 +1,16 @@
+resource "aws_nat_gateway" "ngw1" {
+    allocation_id = aws_eip.eip_1.id
+    subnet_id = aws_subnet.public_1.id 
+    depends_on = [ aws_internet_gateway.igw ]
+    tags = {
+      Name = "${var.app_name}-ngw1"
+    }
+}
+resource "aws_nat_gateway" "ngw2" {
+    allocation_id = aws_eip.eip_2.id 
+    subnet_id = aws_subnet.public_2.id 
+    depends_on = [ aws_internet_gateway.igw ]
+    tags = {
+      Name = "${var.app_name}-ngw2"
+    }
+}
